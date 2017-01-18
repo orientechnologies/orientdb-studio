d5fe205ce11b77f8273a6812b5766857fba65247
Roberto Franchini <ro.franchini@gmail.com>
2017-01-18 12:16:46 +0100

    adds Jenkins for releases

---
+++ Jenkinsfile-release.groovy
@@ -0,0 +1,21 @@
+#!groovy
+node("master") {
+    def mvnHome = tool 'mvn'
+    def mvnJdk8Image = "orientdb/mvn-gradle-node-zulu-jdk-8"
+
+    stage('Source checkout') {
+        checkout scm
+    }
+
+    stage('Run tests on Java8') {
+        docker.image("${mvnJdk8Image}").inside("${env.VOLUMES}") {
+
+            sh "${mvnHome}/bin/mvn  versions:set -DnewVersion=2.2.15"
+            sh "${mvnHome}/bin/mvn  versions:commit"
+            sh "${mvnHome}/bin/mvn  clean install deploy -DperformRelease=true -DskipTests=true -DcreateChecksum=true -Psonatype"
+
+        }
+    }
+
+}
+
