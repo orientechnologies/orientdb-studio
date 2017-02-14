ac2291e95940e7939a59fbe3645713e356bfcb6b
G4br13l3 <gabriele.ponzi@gmail.com>
2017-02-14 19:07:07 +0100

    Teleporter wizard 1st version completed.

--- src/app/app.declarations.ts
+++ src/app/app.declarations.ts
@@ -1,6 +1,8 @@
 import {ImportExportComponent} from './dbconfiguration';
+import {TeleporterComponent} from './administration'
 
 
 export const APP_DECLARATIONS = [
-  ImportExportComponent
+  ImportExportComponent,
+  TeleporterComponent
 ];
