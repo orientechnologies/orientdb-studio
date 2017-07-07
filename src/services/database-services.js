dfbad1a39fbb80b0588fcbcaa94e903e06e04056
maggiolo00 <maggiolo00@gmail.com>
2017-07-07 15:52:01 +0200

    Fixed Database.isEE API with value caching

--- src/services/database-services.js
+++ src/services/database-services.js
@@ -516,6 +516,8 @@ database.factory('DatabaseApi', ["$http", "$resource", "$q", function ($http, $r
   resource.isEE = function () {
 
     let promise;
+
+
     if (resource.ee === null) {
       var deferred = $q.defer();
       $http.get(API + 'isEE').success(function (data) {
@@ -528,9 +530,7 @@ database.factory('DatabaseApi', ["$http", "$resource", "$q", function ($http, $r
       promise = deferred.promise;
       resource.ee = promise;
     } else if (resource.ee.enterprise != undefined) {
-      var deferred = $q.defer();
-      promise = deferred.promise;
-
+      promise = $q.resolve(resource.ee);
     } else {
       promise = resource.ee;
     }
